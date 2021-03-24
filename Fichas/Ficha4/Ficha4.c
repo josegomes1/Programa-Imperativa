e7cf6219af7515c8f62dd94f90cf14245658078e
Zé Pedro Gomes <78722983+josegomes1@users.noreply.github.com>
2021-03-24 00:51:36 +0000

    Update Ficha4.c

--- Fichas/Ficha4/Ficha4.c
+++ Fichas/Ficha4/Ficha4.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 //1.1
 int contaVogais (char *s){
@@ -7,7 +6,7 @@ int contaVogais (char *s){
     int i, count = 0;
     
     //Analisa caracter a caracter até ao fim da string
-    for(i = 0; i < strlen(s); i++){
+    for(i = 0; s[i] != '\0'; i++){
         switch(s[i]){
             case 'a':
                 count++;
@@ -154,14 +153,142 @@ int duplicaVogais2 (char *s){
                for(j = i; s[j] != '\0' ; j++){
 
                    s[j+1] = s[j];
-                   
+
                }
         }
     }
     return count;
 }
 
+//2.1
+int ordenado ( int v[], int N){
+    int i , count = 0;
+
+    for(i = 0; i < N ; i++){
+        if(v[i+1] < v [i]){
+            count++;
+        }
+    }
+    // se count for diferente de 0 é porque não está ordenado e tem count valores fora do lugar
+    return count;
+}
+
+// 2.2
+
+void merge (int a[], int na, int b[], int nb, int r[]){
+    int i = 0, j = 0 , k = 0; 
+
+    while(i != na && j != nb){
+        if(i == na){
+            r[k] = b[j];
+            k++;
+            j++;
+        }else if(j == nb){
+            r[k]= a[i];
+            i++;
+            k++;
+        }else if( a[i] <= b[j] ){
+            r[k] = a[i];
+            i++;
+            k++;
+        }else{
+            r[k] = b[j];
+            j++;
+            k++;
+        }
+    }
+}
+
+// 2.3.a
+
+int partition (int v[], int N, int x){
+
+int i;
+int aux[30] , j= 0;
+int k = 0 , count = 0;
+
+// Count é a quantidade de nºs do array maiores que x , vai servir para calcular posições
+
+while(k < N){
+
+    if(v[k] > x){
+
+        count++;
+
+    }
+    k++;
+}
+
+// copiar para a auxiliar tudo certo sem o x
+
+    for(i = 0 ; i < N ; i++){
+        if(v[i] <= x){
+
+            aux[j] = v[i];
+
+            j++;
+
+        }else{
+
+            aux[N - count] = v[i];
+
+            count--;
+        }
+    }
+
+    for(k = 0 ; k < N ; k++){
+
+        v[k] = aux[k];
+
+    }
+    
+    return j;
+}
+//funções uteis
+void swap (int v[], int i, int j){
+    int temp = v[i];
+
+    v[i] = v[j];
+
+    v[j] = temp;
+}
+
+
+int bubble (int v[] , int N){
+    int i;
+    int r = 1;
+
+    for(i=0; i < N-1 ; i++)
+
+        if(v[i]> v [i+1]) { swap (v,i,i+1); r=0;}
+
+    return r;
+}
+
+void bubbleSort (int v[], int N) {
+    int i;
+    int ord = 0;
+
+    for(i=0; i < N-1 && ! ord; i++)
+        ord = bubble (v,N);
+}
+//2.3.b
+
+int partition2 (int v[], int N, int x){
+
+    bubbleSort(v , N);
+
+    int conta_menores = 0;
+
+    for(int i = 0 ; v[i] <= x ; i ++){
+        conta_menores++;
+    }
+
+    return conta_menores;
+
+}
+
 
 void main() {
-  
+
 }
